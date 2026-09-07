1cac6e3b619e534631f49ce018637999a14b3577
wurendao007 <3055786053@qq.com>
2026-09-07 08:14:39 +0800

    fix(canvas): remove image url copy action

--- web/src/components/canvas/canvas-context-menu.tsx
+++ web/src/components/canvas/canvas-context-menu.tsx
@@ -202,7 +202,7 @@ export function CanvasNodeContextMenu({
                                     <MenuSection label="节点" />
                                     <MenuButton icon={<Copy />} label="复制节点" shortcut="⌘C" onClick={() => runAction(onCopyNode)} />
                                     {isImage ? <MenuButton icon={<Clipboard />} label="复制图片" disabled={!hasNodeContent} onClick={() => runAction(onCopyContent)} /> : null}
-                                    <MenuButton icon={<Link2 />} label={isImage ? "复制图片地址" : "复制视频地址"} disabled={!canCopyMediaUrl} onClick={() => runAction(onCopyMediaUrl)} />
+                                    {!isImage ? <MenuButton icon={<Link2 />} label="复制视频地址" disabled={!canCopyMediaUrl} onClick={() => runAction(onCopyMediaUrl)} /> : null}
                                     <MenuButton icon={<Copy />} label="创建生成副本" onClick={() => runAction(onCreateGenerationCopy)} />
                                     <MenuButton icon={<Layers3 />} label="创建参数变体" shortcut="⌘D" onClick={() => runAction(onDuplicate)} />
                                     <MenuButton icon={<Trash2 />} label="删除节点" danger onClick={() => runAction(onDelete)} />
